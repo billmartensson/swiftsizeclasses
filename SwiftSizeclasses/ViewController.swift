1797cf624501fe089a8348ec296196c95476b903
Bill Mårtensson <bill@magictechnology.se>
2020-09-26 17:50:44 +0200

    Start

--- SwiftSizeclasses/ViewController.swift
+++ SwiftSizeclasses/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func letsClick(_ sender: Any) {
+        
+        if(theLabel.text == "Klick")
+        {
+            theLabel.text = "ABC"
+        } else {
+            theLabel.text = "Klick"
+        }
+        
+    }
+    
 }
 
